407ead97edffd35b57792cb09a3a6a9a8fd95cf3
TXT-53 <txt-53@macbook-pro-9.home>
2020-07-22 21:29:36 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TXT-Angular';
+
+  onSubmit(){
+    var title = (<HTMLInputElement>document.getElementById('blogTitle')).value;
+    console.log(title);
+    document.getElementById('blogsTitle').innerHTML= title;
+
+    var content = (<HTMLInputElement>document.getElementById('blogContent')).value;
+    console.log(content);
+    document.getElementById('blogsContent').innerHTML= content;
+  }
 }
